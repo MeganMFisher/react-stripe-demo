9fcc278e8ec23f3ca4bda6821753cf4e1f859f90
MeganMFisher <meganmichelle318@gmail.com>
2017-07-22 08:26:18 -0600

    testing

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StripeCheckout from 'react-stripe-checkout';
-// import stripeKey from './config';
 import axios from 'axios';
 
 
