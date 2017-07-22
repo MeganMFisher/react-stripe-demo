ded3730333cff946b0d219fa7fc53d086499d6aa
MeganMFisher <meganmichelle318@gmail.com>
2017-07-22 08:13:34 -0600

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StripeCheckout from 'react-stripe-checkout';
+// import stripeKey from './config';
+import axios from 'axios';
+
+
+
+export default class App extends Component {
+  onToken = (token) => {
+    token.card = void 0;
+    console.log('token', token);
+    axios.post('http://localhost:3012/api/payment', { token, amount: 100 } ).then(response => {
+      alert('Heyooo')
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Stripe</h2>
+         </div>
+         <StripeCheckout
+          token={this.onToken}
+          stripeKey="pk_test_8LXvvStMTGkpEl1wGDjpS6Jz"
+          amount={1000}
+        />  
+      </div>
+    );
+  }
+}
+
